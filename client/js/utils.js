9d22dbcee9c6a6b1c643f25b4b3ea109ac0fb14a
Alameen688 <ogundiran12@gmail.com>
2018-08-03 09:50:54 +0100

    add

--- client/js/utils.js
+++ client/js/utils.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars, no-plusplus */
-const baseUrl = 'http://mydiaryoxygen.herokuapp.com/api/v1';
+const baseUrl = 'https://mydiaryoxygen.herokuapp.com/api/v1';
 /** WORKING WITH COOKIES REFERENCE https://www.w3schools.com/js/js_cookies.asp * */
 const getCookie = (cname) => {
   const name = `${cname}=`;

bc73aae664a81ac05b4a093fed72c0e41bddecf9
Alameen688 <ogundiran12@gmail.com>
2019-12-04 23:24:50 +0100

    Narrow down random images with collections - use id for collections 'Minimalist', 'Traveling', 'Cool wallpapers'

--- server/utils/unsplash.js
+++ server/utils/unsplash.js
@@ -8,13 +8,15 @@ export const getRandomImages = async () => {
   let images = [];
   try {
     // Unsplash has limit of 30 count and we would like to generate up to 90 images
-    const responses = await Promise.all([unsplash.photos.getRandomPhoto({ count: 30 }),
-      unsplash.photos.getRandomPhoto({ count: 30 }),
-      unsplash.photos.getRandomPhoto({ count: 30 })]);
+    const responses = await Promise.all([unsplash.photos.getRandomPhoto({ count: 30, collections: ['431862'] }),
+      unsplash.photos.getRandomPhoto({ count: 30, collections: ['632843'] }),
+      unsplash.photos.getRandomPhoto({ count: 30, collections: ['1111678'] })]);
 
     if (responses[0].status === 200) {
       const results = await Promise.all(_.map(responses, response => response.json()));
       images = _.map(Array.prototype.concat(...results), image => image.urls.regular);
+      console.log(images);
+      
     }
   } catch (error) {
     // silence
